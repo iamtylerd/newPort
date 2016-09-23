6d239d14544bea40170777b0c9ab7f309553239d
iamtylerd <tylerdanielphotography@gmail.com>
2016-09-23 14:05:12 -0500

    added menu

--- app/controllers/NavCtrl.js
+++ app/controllers/NavCtrl.js
@@ -7,9 +7,7 @@ var controller = new ScrollMagic.Controller();
 
 // create a scene
 new ScrollMagic.Scene({
-	// triggerElement: '#triggerScrolling'
-        // duration: 100,  // the scene should last for a scroll distance of 100px
-        offset: $(window).height()     // start this scene after scrolling for 50px
+     offset: $(window).height()     // start this scene after scrolling for 100vh
     })
     .setPin("#stickyNav") // pins the element for the the scene's duration
     .addTo(controller); // assign the scene to the controller
